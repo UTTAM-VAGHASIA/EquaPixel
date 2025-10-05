6ae02fc76cf4ebf4ff2276774838d955edcb4db7
UTTAM-VAGHASIA <the.uttam.vaghasia@gmail.com>
2025-10-05 18:11:33 +0530

    Refined file picker helper function

--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
 import PIL
 
+from utils.file_picker import open_file_picker, Mode
+
+if __name__ == "__main__":
+    selected_files = open_file_picker(mode="open", multiple=True)
+    print(selected_files)
